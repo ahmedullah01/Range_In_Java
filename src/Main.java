e0652dfe0b7c31b58fb9f6888b833cf92a15af7a
ahmedullah01 <ahmedullah2095@gmail.com>
2025-08-24 16:26:56 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-range r = new range(0,10,2);
-        int si = r.size();
-        System.out.println(si);
-r.setStep(3);
- si = r.size();
-System.out.println(si);
-        System.out.println(r);
-//        for (int val : r) {  // iterator ka kaam ye loop karega
-//            System.out.print(val + " ");
-//        }
-
+Runner App = new Runner();
+App.execute();
     }
 }
---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+public class Runner {
+    public void execute(){
+        range r = new range(0,10,2);
+        int si = r.size();
+        System.out.println(si);
+        r.setStep(3);
+        si = r.size();
+        System.out.println(si);
+        System.out.println(r);
+        for (int val : r) {  // iterator ka kaam ye loop karega
+            System.out.print(val + " ");
+        }
+
+    }
+}
