1fc77169add926aacdf2335f7b5310b9a4f419fc
ahmedullah01 <ahmedullah2095@gmail.com>
2025-08-24 16:17:19 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ range r = new range(0,10,2);
 r.setStep(3);
  si = r.size();
 System.out.println(si);
-//        System.out.println(r);
+        System.out.println(r);
 //        for (int val : r) {  // iterator ka kaam ye loop karega
 //            System.out.print(val + " ");
 //        }
